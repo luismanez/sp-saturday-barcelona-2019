9e4d79decc674ff88fe8f09d8534da37c3e84553
luismanez <luis.manez@outlook.com>
2019-09-10 14:05:38 +0200

    sps bcn literal in ConsoleLogger

--- demos/done/spfx-my-tasks/src/services/ConsoleLogger.ts
+++ demos/done/spfx-my-tasks/src/services/ConsoleLogger.ts
@@ -6,7 +6,7 @@ export default class ConsoleLogger implements ILogger {
     const currentDatetime = new Date();
     const formattedDate = `${currentDatetime.getFullYear()}-${(currentDatetime.getMonth() + 1)}-${currentDatetime.getDate()}`;
     const formattedTime = `${currentDatetime.getHours()}:${currentDatetime.getMinutes()}:${currentDatetime.getSeconds()}`;
-    console.log("\x1b[32m%s\x1b[0m" ,`[SPSMAD] | ${formattedDate} ${formattedTime} | ${message}`);
+    console.log("\x1b[32m%s\x1b[0m" ,`[SPSBCN] | ${formattedDate} ${formattedTime} | ${message}`);
   }
 
 }
